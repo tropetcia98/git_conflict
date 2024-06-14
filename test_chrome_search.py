d08431f32efcf83589d320530385f4fbb6e00630
Roman Tropin <tropetcia95@gmail.com>
2024-06-14 15:38:43 +0300

    change browser

--- test_chrome_search.py
+++ test_chrome_search.py
@@ -1,5 +1,5 @@
 from selene import browser, have
 
-browser.open('https://google.com')
+browser.open('https://duckduckgo.com')
 browser.element('[name=q]').type('yashaka/selene').press_enter()
 browser.element('#search').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
